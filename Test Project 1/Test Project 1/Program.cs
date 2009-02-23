dcd918b2d34c408a665036f212c044fc62ff550d
unknown <jdindia@.(none)>
2009-02-23 13:12:53 -0800

    A test project.

---
+++ Test Project 1/Test Project 1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Test_Project_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is John's Test Project.");
+            Console.ReadLine();
+        }
+    }
+}
